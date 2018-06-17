b03ac5cc932c43b124d1585d16acc81348bb2fd3
Marcus Lind <marcuslind90@gmail.com>
2018-06-17 18:45:10 +0700

    Create order and replace demo data with real order data in API call

--- controllers/front/process.php
+++ controllers/front/process.php
@@ -4,7 +4,8 @@ class CoinbaseProcessModuleFrontController extends ModuleFrontController {
     const API_URL = "https://api.commerce.coinbase.com";
 
     public function postProcess() {
-        $response = $this->createChargePOST();
+        $order = $this->createOrder();
+        $response = $this->createChargePOST($order);
         $response = json_decode($response, true);
 
         // TODO: Improve error handling here...
@@ -16,26 +17,52 @@ class CoinbaseProcessModuleFrontController extends ModuleFrontController {
         }
         elseif (!array_key_exists('hosted_url', $response['data'])) {
             throw new \Exception("hosted_url key was not in the response.");
-        }
+        }   
 
         header('Location: ' . $response['data']['hosted_url']);
+    }
+
+    protected function createOrder() {
+        $cart = $this->context->cart;
+        $customer = new Customer($cart->id_customer);
+        $currency = $this->context->currency;
+        $total = (float)$cart->getOrderTotal(true, Cart::BOTH);
+        
+        $this->module->validateOrder(
+            $cart->id, 
+            Configuration::get('PS_OS_BANKWIRE'), // id_order_state, the status of the order.
+            $total, // Amount to be paid(?)
+            $this->module->displayName, // Payment Method
+            null, // Message
+            [], // Extra vars
+            (int)$currency->id, // Currency ID
+            false, // Dont touch amount 
+            $customer->secure_key // If we should use a secure key or not
+        );
+
+        return new Order($this->module->currentOrder);
     }
 
     /**
      * HTTP POST Call to API that create a charge.
      * @return string Body of HTTP Response
      */
-    protected function createChargePOST() {
+    protected function createChargePOST($order) {
         $apiKey = Configuration::get('COINBASE_API_KEY');
+        $currency = new Currency($order->id_currency);
 
         $ch = curl_init(self::API_URL . "/charges/");
         $data = json_encode([
-            'name' => "The Human Fund", 
-            'description' => "Money For People", 
+            'name' => Configuration::get('PS_SHOP_NAME'), 
+            'description' => "Payment for order #{$order->id}", 
             'pricing_type' => "fixed_price", 
             'local_price' => [
-                'amount' => 100, 
-                'currency' => 'USD'
+                'amount' => $order->total_paid_tax_incl, 
+                'currency' => $currency->iso_code
+            ], 
+            'metadata' => [
+                'customer_id' => (int)$order->id_customer,
+                'order_id' => (int)$order->id,
             ]
         ]);
         curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
